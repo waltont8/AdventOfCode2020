7aaab299b7be7c6e92a1345b6829cdc71b055f66
waltont8 <2472981+waltont8@users.noreply.github.com>
2020-12-15 14:13:40 +0000

    Added a very slow bruteforce for no reason

--- day13_Lib.hs
+++ day13_Lib.hs
@@ -2,9 +2,9 @@ module Lib
     ( someFunc
     ) where
 
-import Data.Ord (comparing)
 import Data.List
 import Data.List.Split (splitOn)
+import Data.Ord (comparing)
 import Math.NumberTheory.Moduli.Chinese (chineseRemainder)
 
 someFunc :: IO ()
@@ -14,6 +14,13 @@ someFunc = do
   let buses = [ (-g, read b) | (g, b) <- zip [0..] $ splitOn "," (input !! 1), b /= "x"]
   putStrLn $ show $ part1 startTime buses
   putStrLn $ show $ chineseRemainder buses
+  let buses2 = [ (g, read b) | (g, b) <- zip [0..] $ splitOn "," (input !! 1), b /= "x"]
+  putStrLn $ show $ slow buses2
 
 part1 :: Integer -> [(Integer, Integer)] -> Integer
 part1 startTime buses = uncurry (*) $ minimumBy (comparing snd) $ map ((\b -> (b, b * (startTime `div` b + 1) - startTime)) . snd ) buses
+
+slow :: [(Integer, Integer)] -> (Integer, Integer)
+slow = foldl (\(start,step) (offset,busNo) -> (stepper start step busNo (if offset == 0 then 0 else (busNo - offset)), busNo*step)) (100000000000000,1)
+
+stepper start step divideBy remainder = if start `mod` divideBy == remainder then start  else stepper (start+step) step divideBy remainder
